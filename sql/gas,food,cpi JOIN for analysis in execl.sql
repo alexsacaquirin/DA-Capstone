0414810c20fb724b68ecc19c5c7e3568238da6f1
Alex <asacaquirin17@gmail.com>
2023-10-25 18:25:46 -0400

    added execl files with formulas

--- sql/gas,food,cpi JOIN for analysis in execl.sql
+++ sql/gas,food,cpi JOIN for analysis in execl.sql
@@ -1,4 +1,4 @@
-select c.a_observation_date as Date , i.a_gas , i.a_eggs , i.a_ground_chuck , i.a_milk , c.a_rate
+select c.a_observation_date as Date , i.a_gas , i.a_eggs , i.a_ground_chuck , i.a_milk , c.a_rate , i.a_oranges
 from impact as i
 JOIN cpi_rate as c
 ON i.a_month = c.a_observation_date
